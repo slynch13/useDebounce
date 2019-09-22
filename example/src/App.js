2dda422ebf96d77e16c0a7dd25d5995a02744c2b
Sean Lynch <slynch@gmail.com>
2019-09-22 15:46:20 -0400

    Working debounce

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,38 @@
-import React from 'react'
-import { useMyHook } from '@slynch13/usedebounce'
+import React, { useState, useEffect } from 'react'
+import { useDebounce } from '@slynch13/usedebounce'
+import { useInterval } from '@slynch13/useinterval'
 
 const App = () => {
-  const example = useMyHook()
+  let [state, setState] = useState(0)
+  let [last, setLast] = useState('')
+  let [count, setCount] = useState(0)
+  const debounced = useDebounce((display) => {
+    setState(x => x + 1)
+    setLast(display)
+    console.log('Executing')
+  }, 3000)
+  let interval = useInterval(() => {
+    console.log('Test')
+    setCount(x => x + 1)
+    debounced()
+  }, 1000)
+
+  useEffect(() => {
+    if (count > 10) {
+      interval.changeTimeout(2000)
+    }
+    if (count > 20) {
+      interval.changeTimeout(3000)
+    }
+    if (count > 30) {
+      interval.changeTimeout(4000)
+    }
+  }, [count])
   return (
     <div>
-      {example}
+      {state}
+      <div>{last}</div>
+      <div>Executed: {count}</div>
     </div>
   )
 }
