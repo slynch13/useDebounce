329fba3a79022a41925d080644062a43bde0308c
Sean Lynch <slynch@gmail.com>
2019-09-23 17:44:37 -0400

    Removed console logs

--- src/index.js
+++ src/index.js
@@ -4,17 +4,14 @@ export const useDebounce = (func, delay) => {
   let timer = useRef()
   let debounced = (...args) => {
     if (timer.current) {
-      console.log('Bounced', delay)
       let oldTimeout = timer.current
       clearTimeout(oldTimeout)
       timer.current = setTimeout(() => {
-        console.log('Test')
         timer.current = undefined
       }, delay)
       return
     }
     timer.current = setTimeout(() => {
-      console.log('Test2')
       timer.current = undefined
     }, delay)
     func(args || [])
